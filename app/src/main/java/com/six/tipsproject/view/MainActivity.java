0de1d26fa9b2052a9d9777880e25ff826c7ec783
hellenxu <heavensxu@gmail.com>
2016-05-27 18:47:47 -0400

    add: multi-circle view.

---
+++ app/src/main/java/com/six/tipsproject/view/MainActivity.java
@@ -0,0 +1,17 @@
+package com.six.tipsproject.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.six.tipsproject.R;
+
+/**
+ * Created by Xiaolin on 2016-05-27.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
