781c2d4966f46941963362e0497d416d5bc3a0e6
hellenxu <heavensxu@gmail.com>
2016-06-23 23:50:44 -0400

    add: ResideMenu.

--- app/src/main/java/com/six/tipsproject/residemenu/MainActivity.java
+++ app/src/main/java/com/six/tipsproject/residemenu/MainActivity.java
@@ -1,19 +1,43 @@
 package com.six.tipsproject.residemenu;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.six.tipsproject.R;
+
+import ca.six.util.L;
 
 /**
  * Created by Xiaolin on 2016-06-16.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ResideMenuLayout resideMenu;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_reside_menu);
+        setContentView(R.layout.act_residemenu_main);
+        TextView tvOpen = (TextView) findViewById(R.id.tv_open);
+        tvOpen.setOnClickListener(this);
+
+        resideMenu = new ResideMenuLayout(this);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout leftMenu = (LinearLayout) inflater.inflate(R.layout.reside_menu, null);
+        resideMenu.attachToActivity(this, leftMenu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_open){
+            resideMenu.openMenu();
+        }
     }
 }
--- app/src/main/java/com/six/tipsproject/residemenu/ResideMenuLayout.java
+++ app/src/main/java/com/six/tipsproject/residemenu/ResideMenuLayout.java
@@ -1,28 +1,39 @@
 package com.six.tipsproject.residemenu;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.six.tipsproject.R;
 
 /**
  * A simple version of ResideMenu
- *
+ * <p/>
  * Created by Xiaolin on 2016-06-16.
  */
 public class ResideMenuLayout extends FrameLayout {
-    private LinearLayout leftMenu;
-    private ScrollView svLeftMenu;
-    private static final int LEFT_MENU_ID = 0;
+    private FrameLayout decorView;
+    private FrameLayout contentView;
+    private LinearLayout subView;
+    private View lefMenu;
+    private Activity contentActivity;
 
     public ResideMenuLayout(Context context) {
         this(context, null);
@@ -32,15 +43,58 @@ public class ResideMenuLayout extends FrameLayout {
         this(context, attrs, 0);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ResideMenuLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        setupMenu(context);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setupMenu(Context context) {
         setBackground(context.getDrawable(R.drawable.menu_bg));
     }
 
-    private void setupLeftMenu(Context context){
+    public void attachToActivity(Activity activity, View menu) {
+        lefMenu = menu;
+        lefMenu.setAlpha(0);
+        contentActivity = activity;
 
+        decorView = (FrameLayout) activity.getWindow().getDecorView(); // decorview[FrameLayout]
+        subView = (LinearLayout) decorView.getChildAt(0);
+        contentView = (FrameLayout) subView.findViewById(android.R.id.content);
+
+        decorView.removeViewAt(0);
+        this.addView(lefMenu);
+        this.addView(subView);
+        decorView.addView(this, 0);
     }
 
+    public void openMenu() {
+        setPivotPoint();
 
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playTogether(ObjectAnimator.ofFloat(lefMenu, ALPHA, 1),
+                ObjectAnimator.ofFloat(contentView, SCALE_X, 0.5f),
+                ObjectAnimator.ofFloat(contentView, SCALE_Y, 0.5f));
+        animSet.setDuration(300);
+            animSet.start();
+    }
+
+    private void setPivotPoint(){
+        DisplayMetrics dm = new DisplayMetrics();
+        contentActivity.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+
+        contentView.setPivotX(1.2f * screenWidth);
+        contentView.setPivotY(.4f * screenHeight);
+    }
+
+    public void closeMenu() {
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.playTogether(ObjectAnimator.ofFloat(lefMenu, ALPHA, 0),
+                ObjectAnimator.ofFloat(contentView, SCALE_X, 1),
+                ObjectAnimator.ofFloat(contentView, SCALE_Y, 1));
+        animSet.setDuration(300);
+        animSet.start();
+    }
 }
