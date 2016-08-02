9454dbf74fd3613793b38b538231a007f0205a30
hellenxu <heavensxu@gmail.com>
2016-08-01 22:39:23 -0400

    add: multiple processes.

---
+++ app/src/main/java/com/six/tipsproject/ipc/IPCThird.java
@@ -0,0 +1,16 @@
+package com.six.tipsproject.ipc;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @copyright six.ca
+ * Created by Xiaolin on 2016-08-01.
+ */
+public class IPCThird extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
