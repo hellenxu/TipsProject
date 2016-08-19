d5ed8a651fcba0877fd43f85da557ded38c8141d
hellenxu <heavensxu@gmail.com>
2016-08-18 23:09:20 -0400

    add: ProgressDialog sample.

--- app/src/main/java/com/six/tipsproject/strings/StringsActivity.java
+++ app/src/main/java/com/six/tipsproject/strings/StringsActivity.java
@@ -1,6 +1,7 @@
 package com.six.tipsproject.strings;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.AbsoluteSizeSpan;
 import android.text.style.StyleSpan;
+import android.view.View;
 import android.widget.TextView;
 
 import com.six.tipsproject.BuildConfig;
@@ -23,6 +25,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class StringsActivity extends Activity {
     private final String TAG = StringsActivity.class.getSimpleName();
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,9 @@ public class StringsActivity extends Activity {
         final String quantityString = res.getQuantityString(R.plurals.buy_kindle, 0);
         final String quantityString1 = res.getQuantityString(R.plurals.buy_kindle, 1, 1);
         final String quantityString2 = res.getQuantityString(R.plurals.buy_kindle, 3, 3);
-        ((TextView)findViewById(R.id.tv_zero)).setText(quantityString);
-        ((TextView)findViewById(R.id.tv_single)).setText(quantityString1);
-        ((TextView)findViewById(R.id.tv_plural)).setText(quantityString2);
+        ((TextView) findViewById(R.id.tv_zero)).setText(quantityString);
+        ((TextView) findViewById(R.id.tv_single)).setText(quantityString1);
+        ((TextView) findViewById(R.id.tv_plural)).setText(quantityString2);
 
         CommonLogger.logD(TAG, quantityString);
         CommonLogger.logD(TAG, quantityString1);
@@ -53,7 +56,7 @@ public class StringsActivity extends Activity {
         content.add("item00");
         content.add("item00");
 
-        for(String item : content){
+        for (String item : content) {
             CommonLogger.logD(TAG, item);
         }
 
@@ -62,5 +65,32 @@ public class StringsActivity extends Activity {
         spanDes.setSpan(new AbsoluteSizeSpan(20, true), 1, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         spanDes.setSpan(new StyleSpan(Typeface.BOLD_ITALIC), 20, 24, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         tvDes.setText(spanDes);
+    }
+
+    public void showProgressDialog(View view) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("Please wait...");
+        dialog.setProgress(0);
+        dialog.setMax(100);
+        dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        dialog.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(dialog.getProgress() <= dialog.getMax()){
+                    try {
+                        Thread.sleep(6000);
+                        dialog.dismiss();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if(dialog.getProgress() == dialog.getMax()){
+                        dialog.dismiss();
+                    }
+                }
+            }
+        }).start();
+
     }
 }
