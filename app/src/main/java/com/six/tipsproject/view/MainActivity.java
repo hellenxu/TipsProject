9d5c233a3e97028b2864c271722b7c345efe26c5
hellenxu <heavensxu@gmail.com>
2016-09-13 23:39:32 -0400

    add: changing color loading bar.

--- app/src/main/java/com/six/tipsproject/view/MainActivity.java
+++ app/src/main/java/com/six/tipsproject/view/MainActivity.java
@@ -10,14 +10,29 @@ import com.six.tipsproject.R;
  * Created by Xiaolin on 2016-05-27.
  */
 public class MainActivity extends Activity {
+    private ColorfulLoadingView loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.act_circle_menu);
 //        CircleMenuViewGroup circleMenu = (CircleMenuViewGroup) findViewById(R.id.cm_sample);
-        setContentView(R.layout.act_gif);
-        PlayGifView playGifView = (PlayGifView) findViewById(R.id.pgContent);
-        playGifView.setMovieResId(R.drawable.bb8);
+//        setContentView(R.layout.act_gif);
+//        PlayGifView playGifView = (PlayGifView) findViewById(R.id.pgContent);
+//        playGifView.setMovieResId(R.drawable.bb8);
+        setContentView(R.layout.act_color_loading);
+        loadingView = ((ColorfulLoadingView)findViewById(R.id.loading));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadingView.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        loadingView.stop();
     }
 }
