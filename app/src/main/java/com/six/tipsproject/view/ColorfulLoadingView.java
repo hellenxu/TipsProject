62e62f8880c3056e9ec987e147687f3d4be041a7
hellenxu <heavensxu@gmail.com>
2016-09-14 23:49:04 -0400

    add: change text color.

--- app/src/main/java/com/six/tipsproject/view/ColorfulLoadingView.java
+++ app/src/main/java/com/six/tipsproject/view/ColorfulLoadingView.java
@@ -3,6 +3,8 @@ package com.six.tipsproject.view;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -32,11 +34,12 @@ public class ColorfulLoadingView extends View implements Animatable {
     private int maxWidth;
     private String currentProgress;
     private int textX, textY;
+    private Bitmap originalBitmap, progressBitmap;
 
     private static final int DEFAULT_LOADING_COLOR = Color.parseColor("#57d2f7");
     private static final int DEFAULT_PAUSE_COLOR = Color.parseColor("#726dd1");
     private static final int DEFAULT_TEXT_SIZE = 12; //dp
-    private static final int DEFAULT_HEIGHT = 30; //dp
+    private static final int DEFAULT_HEIGHT = 40; //dp
     private static final String DOWNLOAD_LABEL = "Download ";
 
     public ColorfulLoadingView(Context context) {
@@ -57,6 +60,7 @@ public class ColorfulLoadingView extends View implements Animatable {
     private void initValue(Context context, AttributeSet attrs) {
         winManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         metrics = new DisplayMetrics();
+        originalBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.flicker);
 
         final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ColorLoadingBar);
         loadingColor = ta.getColor(R.styleable.ColorLoadingBar_loadingColor, DEFAULT_LOADING_COLOR);
@@ -88,23 +92,23 @@ public class ColorfulLoadingView extends View implements Animatable {
         loadingAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                computeProgress((float)animation.getAnimatedValue());
+                computeProgress((float) animation.getAnimatedValue());
                 invalidate();
             }
         });
     }
 
-    private void computeProgress(float percentage){
+    private void computeProgress(float percentage) {
         currentWidth = (int) (percentage * maxWidth);
-        currentProgress = String.valueOf((int) (percentage * 100)) + "%";
+        currentProgress = DOWNLOAD_LABEL + String.valueOf((int) (percentage * 100)) + "%";
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         maxWidth = getWidth();
-        textX = getWidth() / 2;
-        textY = getHeight() / 2;
+        textX = (int) ((getWidth() - textPaint.measureText(currentProgress)) / 2);
+        textY = (int) ((getHeight() - textPaint.ascent()) / 2);
     }
 
     @Override
@@ -140,7 +144,19 @@ public class ColorfulLoadingView extends View implements Animatable {
         canvas.drawRect(0, 0, currentWidth, getHeight(), progressPaint);
 
         //step 3: draw text
-        canvas.drawText(DOWNLOAD_LABEL + currentProgress, textX, textY, textPaint);
+        textPaint.setColor(loadingColor);
+        canvas.drawText(currentProgress, textX, textY, textPaint);
+
+        if (currentWidth > textX) {
+            canvas.save();
+            canvas.clipRect(textX, 0, Math.min(currentWidth, textX + textPaint.measureText(currentProgress)), getHeight());
+            textPaint.setColor(Color.WHITE);
+            canvas.drawText(currentProgress, textX, textY, textPaint);
+            canvas.restore();
+        }
+
+        //step 4: draw image
+        canvas.drawBitmap(originalBitmap, currentWidth, 0, null);
     }
 
     @Override
