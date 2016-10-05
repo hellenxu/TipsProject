b2926d54c77d0b2ed18dd6f66ffb8b712d2f0b1d
hellenxu <heavensxu@gmail.com>
2016-10-04 23:24:03 -0400

    add: test void method, exception within method samples.

---
+++ app/src/main/java/com/six/tipsproject/mockito/MyString.java
@@ -0,0 +1,13 @@
+package com.six.tipsproject.mockito;
+
+/**
+ * @copyright six.ca
+ * Created by Xiaolin on 2016-10-04.
+ */
+
+public class MyString {
+
+    void print(){
+        System.out.print("MyString.print()");
+    }
+}
