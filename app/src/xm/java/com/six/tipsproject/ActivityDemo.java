2d5b38f26ebddc2ae4b2cbde8f07eeeee0dd66d2
hellenxu <heavensxu@gmail.com>
2016-12-05 22:54:24 -0500

    add: build variant inheritance sample.

---
+++ app/src/xm/java/com/six/tipsproject/ActivityDemo.java
@@ -0,0 +1,18 @@
+package com.six.tipsproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @copyright six.ca
+ * Created by Xiaolin on 2016-12-05.
+ */
+
+public class ActivityDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_demo);
+    }
+}
