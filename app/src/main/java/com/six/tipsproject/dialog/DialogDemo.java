60688dc71aacd6d2691662984095dd358cecde17
hellenxu <heavensxu@gmail.com>
2017-01-05 22:40:10 -0500

    add: some test about theme.

--- app/src/main/java/com/six/tipsproject/dialog/DialogDemo.java
+++ app/src/main/java/com/six/tipsproject/dialog/DialogDemo.java
@@ -2,7 +2,9 @@ package com.six.tipsproject.dialog;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -24,12 +26,34 @@ public class DialogDemo extends Activity {
     public void showDialog(View view){
         final AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.LoadingDialog);
         View dilaogView = LayoutInflater.from(this).inflate(R.layout.dialog_floating, null);
-        builder.setView(dilaogView);
+        builder.setView(dilaogView).setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
         builder.create().show();
 
         View parentView = (View)dilaogView.getParent();
         View superParent = (View) parentView.getParent();
         superParent.setBackgroundColor(getResources().getColor(android.R.color.transparent));
 //        parentView.setBackground(null);
+
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        System.out.println(telephonyManager.getSimSerialNumber());
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("xxl-onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("xxl-onDestroy");
     }
 }
