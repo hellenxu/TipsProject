6b76e6bb3ca0c91bda12885b2340c30a5afd8459
hellenxu <heavensxu@gmail.com>
2017-09-06 15:58:30 -0400

    review LocalBroadcastManager.

---
+++ app/src/main/java/com/six/tipsproject/broadcast/BroadcastDemo.java
@@ -0,0 +1,33 @@
+package com.six.tipsproject.broadcast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+
+/**
+ * @copyright six.ca
+ * Created by Xiaolin on 2017-09-06.
+ */
+
+public class BroadcastDemo extends Activity {
+    private LocalBroadcastManager localBroadcastManager;
+    private SixBroadcastReceiver sixBroadcastReceiver;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        sixBroadcastReceiver = new SixBroadcastReceiver();
+        localBroadcastManager.registerReceiver(sixBroadcastReceiver, new IntentFilter("ca.six.tips"));
+        localBroadcastManager.sendBroadcast(new Intent("ca.six.tips"));
+    }
+
+    @Override
+    protected void onDestroy() {
+        localBroadcastManager.unregisterReceiver(sixBroadcastReceiver);
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/six/tipsproject/broadcast/SixBroadcastReceiver.java
+++ app/src/main/java/com/six/tipsproject/broadcast/SixBroadcastReceiver.java
@@ -22,28 +22,30 @@ public class SixBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //noinspection HardCodedStringLiteral
-        Log.d("xxl", "connectivity change");
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent("com.six.tips.pending"), PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder.setSmallIcon(R.drawable.home)
-                .setContentTitle("Boot Completed")
-                .setContentText("Please enjoy your journey.")
-                .setAutoCancel(true)
-                .setOngoing(true)
-                .setContentIntent(pendingIntent);
+        if (intent.getAction().equals("ca.six.tips")) {
+            System.out.println("received broadcast sent by local manager...");
+        } else {
+            //noinspection HardCodedStringLiteral
+            Log.d("xxl", "connectivity change");
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent("com.six.tips.pending"), PendingIntent.FLAG_UPDATE_CURRENT);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setSmallIcon(R.drawable.home)
+                    .setContentTitle("Boot Completed")
+                    .setContentText("Please enjoy your journey.")
+                    .setAutoCancel(true)
+                    .setOngoing(true)
+                    .setContentIntent(pendingIntent);
 
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
 
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        Log.d("xxl", "telephony: " + telephonyManager.isNetworkRoaming());
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            Log.d("xxl", "telephony: " + telephonyManager.isNetworkRoaming());
 
 //        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 //        Log.d("xxl", "networkInfo: " + networkInfo.isRoaming());
-
-
+        }
 
     }
 }
