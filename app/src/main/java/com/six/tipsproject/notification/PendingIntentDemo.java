5a2047c79994f0ff6b89ecef73b4e830c8bab0d9
hellenxu <heavensxu@gmail.com>
2018-09-08 10:28:55 -0400

    Modified: update gradle and clean up.

--- app/src/main/java/com/six/tipsproject/notification/PendingIntentDemo.java
+++ app/src/main/java/com/six/tipsproject/notification/PendingIntentDemo.java
@@ -5,8 +5,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.view.View;
 
 import com.six.tipsproject.R;
