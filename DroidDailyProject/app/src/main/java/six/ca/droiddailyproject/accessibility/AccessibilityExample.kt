b92981031d18c6325cda7778efac1760ff639cf5
hellenxu <heavensxu@gmail.com>
2019-04-04 23:42:17 -0400

    Add: accessibility simple example.

---
+++ DroidDailyProject/app/src/main/java/six/ca/droiddailyproject/accessibility/AccessibilityExample.kt
@@ -0,0 +1,17 @@
+package six.ca.droiddailyproject.accessibility
+
+import android.app.Activity
+import android.os.Bundle
+import six.ca.droiddailyproject.R
+
+/**
+ * @CopyRight six.ca
+ * Created by Heavens on 2019-04-04.
+ */
+class AccessibilityExample: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_accessibility)
+    }
+}
