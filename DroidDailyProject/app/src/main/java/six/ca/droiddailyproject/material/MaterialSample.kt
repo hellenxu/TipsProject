523f99a8654004b6c570372a18f179570277f739
hellenxu <heavensxu@gmail.com>
2020-06-16 20:25:39 -0400

    add toolbar.

---
+++ DroidDailyProject/app/src/main/java/six/ca/droiddailyproject/material/MaterialSample.kt
@@ -0,0 +1,19 @@
+package six.ca.droiddailyproject.material
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import six.ca.droiddailyproject.R
+
+/**
+ * @author hellenxu
+ * @date 2020-06-16
+ * Copyright 2020 Six. All rights reserved.
+ */
+class MaterialSample: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_material_action_view)
+    }
+}
